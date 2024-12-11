6ef210d6e0f94bb90db010cbf36bb44bc2c5b119
youngjin <bryjna07@nate.com>
2024-12-11 13:20:05 +0900

    feat: 상세기능 구현

--- PoketPhoneBook/PoketPhoneBook/Controllers/PhoneBookViewController.swift
+++ PoketPhoneBook/PoketPhoneBook/Controllers/PhoneBookViewController.swift
@@ -17,9 +17,14 @@ final class PhoneBookViewController: UIViewController {
     // 네트워크 매니저 (싱글톤)
     let networkManager = NetworkManager.shared
     // 코어데이터 매니저
-    let memberListManager = CoreDataManager.shared
+    let coreDataManager = CoreDataManager.shared
     
-    var memberData: MemberData?
+    // MemberData를 전달받을 변수 (전달 받으면 ==> 표시하는 메서드 실행)
+    var memberData: MemberData? {
+        didSet {
+            configureSelectedData()
+        }
+    }
     
     override func loadView() {
         self.view = phoneBookView
@@ -29,7 +34,7 @@ final class PhoneBookViewController: UIViewController {
         super.viewDidLoad()
         setupNaviBar()
         setup()
-        configureSelectedData()
+        
     }
     
     func setup() {
@@ -38,30 +43,29 @@ final class PhoneBookViewController: UIViewController {
         
         phoneBookView.randomButton.addTarget(self, action: #selector(randomButtonTapped), for: .touchUpInside)
     }
-    
+    // 정보 유무에 따른 네비바버튼 이름 변경
     func setupNaviBar() {
-        title = "연락처 추가"
         if memberData != nil {
             let updateButton = UIBarButtonItem(title: "수정", style: .plain, target: self, action: #selector(updateButtonTapped))
             self.navigationItem.rightBarButtonItem = updateButton
         } else {
             let applyButton = UIBarButtonItem(title: "적용", style: .plain, target: self, action: #selector(applyButtonTapped))
+            title = "연락처 추가"
             self.navigationItem.rightBarButtonItem = applyButton
         }
     }
     
     // 선택된 셀의 정보를 받아 UI 표시하기
     func configureSelectedData() {
-        title = memberData?.name
-        phoneBookView.nameTextField.text = memberData?.name
-        phoneBookView.phoneNumberTextField.text = memberData?.phoneNumber
-        // 변환한 이미지를 메인이미지에 담기
-        if let imageData = memberData?.image, let image = UIImage(data: imageData) {
-            phoneBookView.randomImageView.image = image
-        } else {
-            phoneBookView.randomImageView.image = UIImage(named: "ball") // 기본 이미지 설정
-        }
+        
         if memberData != nil {
+            title = memberData?.name
+            phoneBookView.nameTextField.text = memberData?.name
+            phoneBookView.phoneNumberTextField.text = memberData?.phoneNumber
+            if let imageData = memberData?.image, let image = UIImage(data: imageData) {
+                phoneBookView.randomImageView.image = image
+            }
+            // 정보가 있을 시 삭제버튼 생성
             let deleteButton: UIButton = {
                 let button = UIButton()
                 button.setTitle("삭제", for: .normal)
@@ -75,20 +79,29 @@ final class PhoneBookViewController: UIViewController {
                 make.top.equalTo(phoneBookView.phoneNumberTextField.snp.bottom).offset(20)
                 make.centerX.equalToSuperview()
             }
+            
         }
     }
     
+    // MARK: - 버튼 관련
     
     @objc func applyButtonTapped() {
         //적용버튼 눌렀을 때
         let name = phoneBookView.nameTextField.text
-        let phone = phoneBookView.phoneNumberTextField.text
+        let phone = phoneBookView.phoneNumberTextField.text?.withHypen
         let image = phoneBookView.randomImageView.image
-        // 이미지의 경우 JPEG로 변환하여 코어데이터에 저장 후 pop
-        let imageData = image?.jpegData(compressionQuality: 0.5)
-        memberListManager.saveMemberData(name: name, phone: phone, image: imageData) {
-            print("저장 완료")
-            self.navigationController?.popViewController(animated: true)
+        if (name?.count != 0) && (phone!.count > 9) && image != nil {
+            // 이미지의 경우 JPEG로 변환하여 코어데이터에 저장 후 pop
+            let imageData = image?.jpegData(compressionQuality: 0.5)
+            coreDataManager.saveMemberData(name: name, phone: phone, image: imageData) {
+                print("저장 완료")
+                self.navigationController?.popViewController(animated: true)
+            }
+        } else {
+            let alert = UIAlertController(title: "적용 실패", message: "올바른 정보를 입력해주세요!", preferredStyle: .alert)
+            let succes = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(succes)
+            present(alert, animated: true)
         }
     }
     // 수정 버튼 클릭 시
@@ -96,9 +109,9 @@ final class PhoneBookViewController: UIViewController {
         
         if let memberData = self.memberData {
             memberData.name = phoneBookView.nameTextField.text
-            memberData.phoneNumber = phoneBookView.phoneNumberTextField.text
+            memberData.phoneNumber = phoneBookView.phoneNumberTextField.text?.withHypen
             memberData.image = phoneBookView.randomImageView.image?.jpegData(compressionQuality: 0.5)
-            memberListManager.updateMember(newMemberData: memberData) {
+            coreDataManager.updateMember(newMemberData: memberData) {
                 print("수정 완료")
                 self.navigationController?.popViewController(animated: true)
             }
@@ -106,14 +119,24 @@ final class PhoneBookViewController: UIViewController {
     }
     // 삭제 버튼 클릭 시
     @objc func deleteButtonTapped() {
-        if let memberData = self.memberData {
-            memberListManager.deleteMember(data: memberData) {
-                print("삭제 완료")
-                self.navigationController?.popViewController(animated: true)
+        // 알럿창 띄우기 - 확인 클릭 시 삭제
+        let alert = UIAlertController(title: "연락처 삭제", message: "정말 삭제하시겠습니까?", preferredStyle: .alert)
+        let succes = UIAlertAction(title: "확인", style: .default) { action in
+            if let memberData = self.memberData {
+                self.coreDataManager.deleteMember(data: memberData) {
+                    print("삭제 완료")
+                    self.navigationController?.popViewController(animated: true)
+                }
             }
         }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(succes)
+        alert.addAction(cancel)
+        self.present(alert, animated: true)
     }
-        // MARK: - 랜덤 이미지 받아오기
+    
+    
+    // MARK: - API 랜덤 이미지 받아오기
     @objc func randomButtonTapped(_ sender: UIButton) {
         fetchPoketmonData()
     }
@@ -183,15 +206,33 @@ extension PhoneBookViewController: UITextFieldDelegate {
     
     // 텍스트필드에 글자내용이 변할때마다 관련 메서드
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // 숫자가 안지워져서 추가한 코드
         if string.isEmpty {
             return true
         }
         if textField == phoneBookView.nameTextField {
             return Int(string) == nil // 이름 필드에 숫자 입력 안되게
         } else if textField == phoneBookView.phoneNumberTextField {
+            // 11글자이상 입력되는 것을 막는 코드
+            if (textField.text?.count)! + string.count > 11 {
+                return false
+            }
             return Int(string) != nil // 전화번호 필드에 문자 입력 안되게
         }
         return true
     }
     
+}
+// MARK: - 휴대폰 번호 하이픈 추가
+extension String {
+    
+    public var withHypen: String {
+        var stringWithHypen: String = self
+        guard self.count == 10 || self.count == 11 else { return self } // 10자리나 11자리만 처리
+        
+        stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
+        stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: -4))
+        
+        return stringWithHypen
+    }
 }
